d0484f54aee8b251428c2261ca0f8f8b8b032004
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-14 20:51:21 +0100

    Make project cards into links

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const appify = document.getElementById('appify');
+const kidMath = document.getElementById('kid-math');
+const pixelArt = document.getElementById('pixel-art');
+
+appify.addEventListener('click', () => {
+  window.location.href = '#';
+});
+
+kidMath.addEventListener('click', () => {
+  window.location.href = 'https://github.com/IyiKuyoro/KidMath';
+});
+
+pixelArt.addEventListener('click', () => {
+  window.location.href = 'https://github.com/IyiKuyoro/ALC-3.0-Pixel-Art-Project';
+});
