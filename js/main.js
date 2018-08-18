886d1258868d0c12559a436a03de8c72b6bd7132
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-08-18 20:56:33 +0100

    Add Frogger project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const memoryGame = document.getElementById('memory-game');
 const kidMath = document.getElementById('kid-math');
 const pixelArt = document.getElementById('pixel-art');
+const arcadeGame = document.getElementById('arcade-game');
 
 memoryGame.addEventListener('click', () => {
   // This code was gotten from StackOverflow --> https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window-using-javascript
@@ -15,5 +16,10 @@ kidMath.addEventListener('click', () => {
 
 pixelArt.addEventListener('click', () => {
   const win = window.open('https://github.com/IyiKuyoro/ALC-3.0-Pixel-Art-Project', '_blank');
+  win.focus();
+});
+
+arcadeGame.addEventListener('click', () => {
+  const win = window.open('https://github.com/IyiKuyoro/Arcade-Game', '_blank');
   win.focus();
 });
